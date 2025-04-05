5b03ddaae9c1b0b8279d1c4a66bd8498caee432f
Deepshikha <deepshikha.jain.coox@gmail.com>
2025-04-05 12:24:10 +0530

    donewe

--- server.js
+++ server.js
@@ -7,24 +7,30 @@ require('dotenv').config();
 
 const app = express();
 
+
 connectDB();
+
 const corsOptions = {
-  origin: '*',
+  origin: 'http://localhost:3000',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
 };
 
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.options('*', cors(corsOptions)); 
+
 
 app.use(express.json());
 
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
+
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/blogs', require('./routes/blogRoutes'));
+
 
 app.use((err, req, res, next) => {
   if (err instanceof ValidationError) {
@@ -40,5 +46,6 @@ app.use((err, req, res, next) => {
   });
 });
 
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Running on port ${PORT}`));
